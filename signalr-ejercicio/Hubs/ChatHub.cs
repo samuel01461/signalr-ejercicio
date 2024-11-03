c86f983ce39cc8b5a6bb13703c02f4f707b2c0d9
samuel01461 <samuelgithub5@gmail.com>
2024-11-03 08:29:13 -0300

    first commit

---
+++ signalr-ejercicio/Hubs/ChatHub.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace signalr_ejercicio.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private const string room = "principal";
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            UsersManager.AgregarUsuario(Context.ConnectionId);
+            var usuarios = ObtenerUsuarios();
+            await Clients.Group(room).SendAsync("updateListaUsuarios", usuarios);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, room);
+            UsersManager.RemoverUsuario(Context.ConnectionId);
+            var usuarios = ObtenerUsuarios();
+            await Clients.Group(room).SendAsync("updateListaUsuarios", usuarios);
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public List<string> ObtenerUsuarios()
+        {
+            return UsersManager.ObtenerUsuarios();
+        }
+        public async Task EnviarMensaje(string mensaje)
+        {
+            var emisor = Context.ConnectionId;
+            await Clients.Group(room).SendAsync("RecibirMensaje", emisor, mensaje);
+        }
+
+        public async Task EnviarMensajePrivado(string receptor, string mensaje)
+        {
+            var emisor = Context.ConnectionId;
+            await Clients.Client(receptor).SendAsync("RecibirMensajePrivado", emisor, mensaje);
+        }
+    }
+}
---
+++ signalr-ejercicio/UsersManager/UsersManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+
+public class UsersManager
+{
+    private static readonly ConcurrentDictionary<string, string> _users = new();
+
+    public static void AgregarUsuario(string connectionId)
+    {
+        _users[connectionId] = Guid.NewGuid().ToString();
+    }
+
+    public static void RemoverUsuario(string connectionId)
+    {
+        _users.TryRemove(connectionId, out _);
+    }
+
+    public static List<string> ObtenerUsuarios()
+    {
+        return _users.Keys.ToList();
+    }
+}
